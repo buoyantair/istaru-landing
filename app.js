1b35b3d1aa2d01eb41f2fe6bff091bdaecd914d5
buoyantair <buoyantair@gmail.com>
2017-04-12 15:31:24 +0530

    Hmm This is weird

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 8080;
+
 app.set('view engine', 'pug');
 app.use(express.static('assets'));
 app.get('/', function (req, res) {
   res.render('index', { pageTitle: 'I Star You - Home'})
 })
 
-app.listen(process.env.port, ()=>{
+app.listen(port, ()=>{
   console.log("We're up and running senpai!")
 })
