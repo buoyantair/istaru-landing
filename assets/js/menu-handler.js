9b18ebe2636dfe8f742d72c82acbd5ba4d0f00c3
buoyantair <buoyantair@gmail.com>
2017-04-13 19:26:47 +0530

    Added parallax to the first section

--- assets/js/menu-handler.js
+++ assets/js/menu-handler.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $(window).on('scroll', function(){
     if(window.scrollY >= 300){
-      $('.top-bar').addClass('pinned')
-    } else if(window.scrollY <= 300){
+      $('.pop-in-l').css("right", "0");
+      $('.pop-in-r').css("left", "0");
+      $('.top-bar').addClass('pinned');
+    }else if(window.scrollY <= 300){
+      $('.pop-in-l').css("right", "100%");
+      $('.pop-in-r').css("left", "100%");
       $('.top-bar').removeClass('pinned')
-    } else if(window.scrollY >= 500){
-      // $('.pop-in-l').css("right", "0");
-      // $('.pop-in-r').css("left", "0");
-      console.log("boom")
     }
   })
 })
